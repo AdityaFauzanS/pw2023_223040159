4c38d11fbcba89cf0e81ba74c53e5cce42d6fc5c
AdityaFauzanS <aditya.223040159@mail.unpas.ac.id>
2023-02-21 19:37:38 +0700

    Tugas Kuliah

---
+++ b.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1b</title>
+</head>
+<body>
+    <?php
+    
+    $angka = 59;
+    echo "saya adalah angka <b>$angka</b><br>";
+    echo "jika saya di kali 5, maka saya sekarang menjadi <b>" . $angka * 5 . "</b><br>";
+    echo "jika saya di bagi 2, maka saya sekarang menjadi <b>" . $angka / 2 . "</b><br>";
+    echo "jika saya di tambah 75, maka saya sekarang menjadi <b>" . $angka + 75 . "</b><br>";
+    echo "jika saya di kurang 20, maka saya sekarang menjadi <b>" . $angka - 20 . "</b>";
+    
+    ?>
+</body>
+</html>
